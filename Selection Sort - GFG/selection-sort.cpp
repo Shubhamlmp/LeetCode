9e38c6d45eadacef2233216e2780bed4589f2172
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-06-27 15:04:27 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.cpp
+++ Selection Sort - GFG/selection-sort.cpp
@@ -22,11 +22,13 @@ class Solution
     {
        //code here
        for(int i = 0 ; i < n; i++){
+           int min = i;
            for(int j = i+1; j < n; j++){
-               if(arr[i] > arr[j]){
-               swap(arr[i], arr[j]);
+               if(arr[min] > arr[j]){
+               min = j;
            }
            }
+           swap(arr[i], arr[min]);
        }
     }
 };
