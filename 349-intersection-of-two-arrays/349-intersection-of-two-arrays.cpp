5c50ccc91b2d70eafcb19e4fd839b8a753c39243
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-07-20 18:08:15 +0530

    Time: 13 ms (33.29%), Space: 9.9 MB (91.93%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -3,17 +3,20 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
+        
         vector<int> ans;
+        int arr[1001] = {-1};
+        
         for(int i = 0; i< n1; i++){
-            for(int j = 0; j < n2; j++){
-                if(nums1[i] == nums2[j]){
-                    ans.push_back(nums1[i]);
-                    break;
-                }
+            arr[nums1[i]] = 1;
+        }
+        
+        for(int i = 0; i< n2; i++){
+            if(arr[nums2[i]] == 1){
+                ans.push_back(nums2[i]);
+                arr[nums2[i]] = -1;
             }
         }
-        set<int> s( ans.begin(), ans.end() );
-        ans.assign( s.begin(), s.end() );
         return ans;
     }
 };
