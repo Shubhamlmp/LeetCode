13c6a258bc5287a9259739a007a70421cfcb23e7
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-07-24 00:15:44 +0530

    Time: 200 ms (90.72%), Space: 10.3 MB (32.72%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        bool* isPrime = new bool[n];
-        
+        vector<bool> seen(n, false);
+        int ans = 0;
         for(int i = 2; i < n; i++){
-            isPrime[i] = true;
-        }
-        
-       for(int i = 2; i*i < n; i++){
-            if(!isPrime[i]){
-                continue;
-            }
-           for(int j = i*i ; j < n; j += i){
-               isPrime[j] = false;
-           }
-        }
-        int count = 0;
-       for(int i = 2; i < n; i++){
-            if(isPrime[i]){
-                count++;
+            if(seen[i]) continue;
+            ans++;
+            for(long j = (long) i*i; j < n; j += i){
+                seen[j] = true;
             }
         }
-        return count;
+        return ans;
     }
 };
