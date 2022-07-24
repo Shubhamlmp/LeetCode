c98494c04156c690a5c784d4f48ce4e4720355a4
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-07-24 19:12:53 +0530

    Time: 87 ms (29.30%), Space: 14.6 MB (72.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -14,8 +14,18 @@ public:
         ListNode* temp2 = headB;
         
         while(temp1 != temp2){
-            temp1 = !temp1 ? headB : temp1->next;
-            temp2 = !temp2 ? headA : temp2->next;
+            if(temp1 == NULL){
+                temp1 = headB;
+            }
+            else{
+                temp1 = temp1->next;
+            }
+           if(temp2 == NULL){
+                temp2 = headA;
+            }
+            else{
+                temp2 = temp2->next;
+            }
         }
         return temp1;
     }
