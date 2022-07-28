9d75cfd3980a994259fe9a2e00cab8fdf722e6ff
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-07-28 13:10:40 +0530

    Time: 34 ms (76.79%), Space: 10.6 MB (90.90%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -4,9 +4,7 @@ public:
         vector<int>v(26,0);
         for(char c : s) v[c-'a']++;
         for(int i = 0; i < s.length(); i++){
-            if(v[s[i] - 'a'] == 1){
-                return i;
-            }
+            if(v[s[i] - 'a'] == 1) return i;
         }
         return -1;
     }
