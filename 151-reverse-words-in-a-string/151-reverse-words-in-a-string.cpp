822d0f5ebd080a988179b900dfd5e5f9fd1addb4
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-07-28 14:56:42 +0530

    Time: 14 ms (33.98%), Space: 7.5 MB (62.93%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        
         if(s.size() == 0) return s;
         stack<string> st;
         string result;
+        
         for(int i = 0; i < s.size(); i++){
             string word;
             if(s[i] == ' ') continue;
@@ -13,11 +15,11 @@ public:
             }
             st.push(word);
         } 
-            while(!st.empty()){
-                result += st.top();
-                st.pop();
-                if(!st.empty()) result += " ";
-            }
+        while(!st.empty()){
+            result += st.top();
+            st.pop();
+            if(!st.empty()) result += " ";
+        }
         return result;
     }
 };
