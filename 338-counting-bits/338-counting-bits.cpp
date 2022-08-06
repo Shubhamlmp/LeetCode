ea4fa48453d60a34ef61b6c47910bb8702457bb9
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-08-06 19:04:04 +0530

    Time: 7 ms (72.11%), Space: 8.6 MB (21.04%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(0);
+        for(int i = 0; i <= n; i++){
+            int count = 0;
+            int num = i;
+            while(num){
+                count += num%2;
+                num /= 2;
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
