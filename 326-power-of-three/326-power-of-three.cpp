7916d05dfca10f6ebaac27ad46a02e56ace98dd4
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-08-24 09:35:15 +0530

    Time: 36 ms (45.15%), Space: 5.9 MB (72.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         long long int i = 1;
+        
         while(i < n){
             i *= 3;
         }
-        return i == n;
+        return i==n;
     }
 };
