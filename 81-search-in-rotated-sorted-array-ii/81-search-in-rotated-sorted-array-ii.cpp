069951c853794423f54be12f953b6e0e1ce698d8
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-09-18 12:11:50 +0530

    Time: 4 ms (90.79%), Space: 14 MB (43.09%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,10 +1,33 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == target){
-                return true;
-                break;
+    bool search(vector<int>& nums, int key) {
+        int l = 0;
+        int h = nums.size()-1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(nums[mid] == key) return true;
+            //for duplicates
+           if(nums[l] == nums[mid] && nums[h] == nums[mid]){
+                l++;
+                h--;
+            }
+            //checking in left half
+            else if(nums[l] <= nums[mid]){
+                if(key >= nums[l] && key <= nums[mid]){
+                    h = mid -1;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+            //checking in right half
+            else{
+                if(key >= nums[mid] && key <= nums[h]){
+                    l = mid+1;
+                }
+                else{
+                    h = mid-1;
+                }
             }
         }
         return false;
