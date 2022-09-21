947d30ee462b4bd3a311b3c3a159d48840e0f2c7
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-09-21 17:43:06 +0530

    Time: 157 ms (5.04%), Space: 19 MB (33.40%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -3,7 +3,7 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1, high = *max_element(piles.begin(),piles.end()), k = 0;
         while(low<=high){
-            k = (low + high)/2;
+            k = low + (high-low)/2;
             long long int H = 0;
             for(int i = 0; i < piles.size(); i++){
                 H += ceil(1.0 * piles[i]/k);
