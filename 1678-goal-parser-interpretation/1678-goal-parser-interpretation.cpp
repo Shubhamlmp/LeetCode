f77c6cdbfda0b7d2643bed79aea93995003080da
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2022-11-03 01:21:39 +0530

    Time: 3 ms (52.87%), Space: 6.1 MB (82.89%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     string interpret(string c) {
         string s = "";
-        for(int i = 0; i < c.size(); i++){
+        int i = 0;
+        while(i < c.size()){
             if(c[i] == 'G'){
                 s += "G";
+                i++;
             }
             else if(c[i] == '(' && c[i+1] == ')'){
                 s += "o";
+                i = i+2;
             }
             else if(c[i] == '(' && c[i+1] == 'a' && c[i+2] == 'l'&& c[i+3] == ')'){
                 s += "al";
+                i = i + 4;
             }
         }
         return s;
