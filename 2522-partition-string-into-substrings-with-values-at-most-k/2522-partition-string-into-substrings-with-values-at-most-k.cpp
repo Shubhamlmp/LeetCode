24fcbaec7c0e08302097aa61c925fdc0ecaf450a
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-01-02 01:20:39 +0530

    Time: 8 ms (100.00%), Space: 9.1 MB (100.00%) - LeetHub

---
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumPartition(string s, int k) {
+        long long n = 0, res = 1;
+        for(int i = 0; i < s.size(); i++){
+            if(n*10 + (s[i]-'0') > k){
+                res++;
+                n = 0;
+            }
+            n = n*10 + (s[i]-'0');
+            if(s[i]-'0' > k) return -1;
+        }
+        return res;
+    }
+};
