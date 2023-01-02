89027be04e28a2cf36150c468edcaf9f34196a95
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-01-02 23:39:19 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (24.57%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        if(word.size() < 2) return true;
+        if(isupper(word[0]) && isupper(word[1])){
+            for(int i = 2; i < word.size(); i++){
+                if(islower(word[i])) return false;
+            }
+        }
+        else {
+            for(int i = 1; i < word.size(); i++){
+                if(isupper(word[i])) {
+                    return false;
+                } 
+            }
+        }
+        return true;
+    }
+};
