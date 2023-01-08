266b2d7e8470043bf1919cb52256fd72e1178931
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-01-09 02:52:45 +0530

    Time: 3 ms (90.16%), Space: 7.1 MB (24.65%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.size() != goal.size()){
+            return false;
+        }
+        
+        if(s == goal){
+            unordered_map<char, int>m;
+            for(auto x : s){
+                m[x]++;
+                if(m[x] > 1){
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        vector<int>diff;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != goal[i]){
+                diff.push_back(i);
+                if(diff.size() > 2){
+                    return false;
+                }
+            }
+        }
+        if(diff.size() != 2){
+            return false;
+        }
+        if(s[diff[0]] == goal[diff[1]] && s[diff[1]] == goal[diff[0]]){
+            return true;
+        }
+        return false;
+    }
+};
