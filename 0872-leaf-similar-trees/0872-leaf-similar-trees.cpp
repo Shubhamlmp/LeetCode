9cfdcaf68baf23ef7e5c1268652fb6609c4650fb
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-02-28 02:14:00 +0530

    Time: 3 ms (79.19%), Space: 12.8 MB (63.88%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,8 +11,10 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int> &v) {
-        if(root == NULL) return;
+    void helper(TreeNode* root, vector<int> &v){
+        if(root == NULL){
+            return;
+        }
         if(root->left == NULL && root->right == NULL){
             v.push_back(root->val);
         }
@@ -20,8 +22,7 @@ public:
         helper(root->right, v);
     }
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int>v1;
-        vector<int>v2;
+        vector<int>v1,v2;
         helper(root1,v1);
         helper(root2,v2);
         return v1 == v2;
