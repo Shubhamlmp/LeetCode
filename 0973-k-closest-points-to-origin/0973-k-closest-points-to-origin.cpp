05a3c9dfcc81186ac32c3517f2cb09bec62186df
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-07-02 01:09:51 +0530

    Time: 234 ms (78.66%), Space: 69.7 MB (56.54%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,16 +1,26 @@
+class Compare{
+public:
+    bool operator()(pair<int,int>a, pair<int,int>b){
+        return a.second >= b.second;
+    }
+};
+
 class Solution {
 public:
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int,vector<int>>> v;
-        for(int i = 0; i < points.size(); i++){
-            int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
-            v.push_back({dist, {points[i][0] , points[i][1]}});
+        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare>pq;
+        int i = 0, a, b;
+        for(auto p : points){
+            a = p[0], b = p[1];
+            pq.push({i++, a*a+b*b});
         }
-        sort(v.begin(),v.end());
+        
         vector<vector<int>> ans;
         int j = 0;
         while(j < k){
-            ans.push_back(v[j].second);
+            ans.push_back(points[pq.top().first]);
+            pq.pop();
             j++;
         }
         return ans;
