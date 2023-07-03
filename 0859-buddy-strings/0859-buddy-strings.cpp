fe24b24e04e96993b4ffaa25f53640c6cd2cee02
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-07-03 12:46:42 +0530

    Time: 3 ms (78.34%), Space: 6.9 MB (85.37%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -4,18 +4,16 @@ public:
         if(s.size() != goal.size()){
             return false;
         }
-        
         if(s == goal){
-            unordered_map<char, int>m;
+            unordered_map<char,int>s1;
             for(auto x : s){
-                m[x]++;
-                if(m[x] > 1){
+                s1[x]++;
+                if(s1[x] > 1){
                     return true;
                 }
             }
             return false;
         }
-        
         vector<int>diff;
         for(int i = 0; i < s.size(); i++){
             if(s[i] != goal[i]){
@@ -25,7 +23,7 @@ public:
                 }
             }
         }
-        if(diff.size() != 2){
+        if(diff.size() < 2){
             return false;
         }
         if(s[diff[0]] == goal[diff[1]] && s[diff[1]] == goal[diff[0]]){
