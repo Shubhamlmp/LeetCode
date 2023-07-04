fad24ea76600395fa5ebed620dbf01c28467c1b6
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-07-04 15:47:20 +0530

    Time: 754 ms (78.10%), Space: 98.8 MB (92.70%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
-        priority_queue<int>pq;
-        for(auto x : piles){
-            pq.push(x);
-        }
+        priority_queue<int>pq(begin(piles), end(piles));
+
         while(k--){
-            int top = 0;
-            if(pq.top()%2 != 0){
-                top = pq.top()/2 + 1;
-            }
-            else{
-                top = pq.top()/2;
-            }
+            int top = pq.top() - pq.top()/2;
             pq.pop();
             pq.push(top);
         }
         int ans = 0;
         while(!pq.empty()){
-            cout << pq.top() << " ";
             ans += pq.top();
             pq.pop();
         }
