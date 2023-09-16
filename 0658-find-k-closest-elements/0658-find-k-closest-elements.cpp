f7676f037c1bf996fd459058e702fb3a834d4307
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-09-16 23:44:38 +0530

    Time: 72 ms (25.54%), Space: 32 MB (44.33%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        stable_sort(arr.begin(),arr.end(), [x](const auto a, const auto b){
+            return abs(a-x) < abs(b-x);
+        });
+        arr.resize(k);
+        sort(arr.begin(),arr.end());
+        return arr;
+    }
+};
