e50855858cfc6ecc58d900f404883ecfa177ff1f
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2023-10-04 14:40:06 +0530

    Time: 150 ms (69.59%), Space: 82.2 MB (61.42%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size() < 2) return 0;
+        sort(nums.begin(),nums.end());
+        int ans = INT_MIN;
+        for(int i = 0; i < nums.size()-1; i++){
+            ans = max(abs(nums[i]-nums[i+1]) , ans);
+        }
+        return ans;
+    }
+};
