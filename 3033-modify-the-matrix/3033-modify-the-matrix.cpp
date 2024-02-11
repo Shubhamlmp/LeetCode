1b85a8b49656cfb3ad0388f6169c75548e76869e
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2024-02-11 19:56:41 +0530

    Time: 9 ms (80.00%), Space: 22.3 MB (80.00%) - LeetHub

---
+++ 3033-modify-the-matrix/3033-modify-the-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        for(int i = 0; i < m; i++){
+            int mx = -9;
+            for(int j = 0; j < n; j++){
+                mx = max(mx, matrix[j][i]);
+            }
+            for(int j = 0; j < n; j++){
+                if(matrix[j][i] == -1){
+                    matrix[j][i] = mx;
+                }
+            }
+        }
+        return matrix;
+    }
+};
