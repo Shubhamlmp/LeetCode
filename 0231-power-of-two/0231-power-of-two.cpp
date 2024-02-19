b6c8ec4ead904f45cebe5f44bd6a7616b60d65ee
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2024-02-19 23:03:30 +0530

    Time: 2 ms (39.05%), Space: 7.2 MB (39.77%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    bool check(int n, long long int i){
-        if(i > n){
+    bool isPowerOfTwo(int n) {
+        if(n <= 0){
             return false;
         }
-        if(n == i){
-            return true;
-        }
-        return check(n, i*2);
-    }
-    bool isPowerOfTwo(int n) {
-        if(n == 1){
-            return true;
-        }
-        return check(n, 2);
+        return ((n&(n-1)) == 0);
     }
 };
