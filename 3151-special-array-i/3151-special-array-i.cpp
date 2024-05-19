f9c85e1b58611ce2daf04d49c43ec6ae572de5c0
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2024-05-19 19:19:49 +0530

    Time: 8 ms (40.00%), Space: 26.7 MB (80.00%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -1,29 +1,9 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        bool ans = true;
-        bool prevOdd = false;
-        bool prevEven = false;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i]%2 == 0){
-                if(prevEven){
-                    return false;
-                }
-                else{
-                    prevEven = true;
-                    prevOdd = false;
-                }
-            }
-            else{
-                if(prevOdd){
-                    return false;
-                }
-                else{
-                    prevOdd = true;
-                    prevEven = false;
-                }
-            }
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i]%2 == nums[i-1]%2) return false;
         }
-        return ans;
+        return true;
     }
 };
