f7923b40f2cbda430f64c80a939573fe6b8c04f1
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2024-06-13 00:37:19 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (5.78%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1){
+            return x;
+        }
+        
+        int start = 0;
+        int end = x;
+        int mid = -1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if((long) mid * mid == x){
+                return mid;
+            } else if((long) mid * mid > x){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return static_cast<int>(round(end));
+    }
+};
