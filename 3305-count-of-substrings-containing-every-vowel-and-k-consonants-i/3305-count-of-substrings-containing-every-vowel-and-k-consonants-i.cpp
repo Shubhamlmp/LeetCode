0da6b8b4308946f09a36a45357c74540b9da9fdd
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2024-09-29 19:55:03 +0530

    Time: 38 ms (45.45%), Space: 20 MB (9.09%) - LeetHub

--- 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -4,38 +4,32 @@ public:
         int ans = 0;
         for(int i = 0; i < word.size(); i++){
             string t = "";
+            int a = 0, e = 0, iNum = 0, o = 0, u = 0;
+            int xyz = 0;
             for(int j = i; j < word.size(); j++){
                 t += word[j];
+                if(word[j] == 'a'){
+                    a++;
+                } else if(word[j] == 'e') {
+                    e++;
+                } else if(word[j] == 'i') {
+                    iNum++;
+                } else if(word[j] == 'o') {
+                    o++;
+                } else if(word[j] == 'u') {
+                    u++;
+                } else {
+                    xyz++;
+                }
                 if(t.size() >= 5){
-                    ans += eligible(t, k);
+                    if(a > 0 && e > 0 && iNum > 0 && o > 0 && u >> 0 && xyz == k){
+                        ans++;
+                    }
+                    
                 }
             }
         }
         return ans;
-    }
-    
-    int eligible(string s, int k){
-        int a = 0, e = 0, i = 0, o = 0, u = 0;
-        int xyz = 0;
-        for(auto x : s){
-            if(x == 'a'){
-                a++;
-            } else if(x == 'e') {
-                e++;
-            } else if(x == 'i') {
-                i++;
-            } else if(x == 'o') {
-                o++;
-            } else if(x == 'u') {
-                u++;
-            } else {
-                xyz++;
-            }
-        }
-        if(a > 0 && e > 0 && i > 0 && o > 0 && u >> 0 && xyz == k){
-            return 1;
-        }
-        return 0;
     }
 };
 
