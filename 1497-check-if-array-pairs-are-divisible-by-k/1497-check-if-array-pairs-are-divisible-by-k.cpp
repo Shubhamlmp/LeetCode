029f3453c2f1b3a3936a36109ee6c7c91b28b703
Shubham Verma <45103446+Shubhamlmp@users.noreply.github.com>
2024-10-02 03:10:30 +0530

    Time: 130 ms (44.87%), Space: 71.4 MB (74.08%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -7,20 +7,22 @@ public:
             if(rem < 0) {
                 rem += k;
             }
-            mp[rem]++;
-        }
-        
-        for(auto x : mp){
-            if(x.first == 0){
-                if(x.second%2 != 0) {
-                    return false;
+            if(rem == 0) {
+                if(mp.count(rem) > 0) {
+                    mp.erase(rem);
                 }
             } else {
-                if(mp[x.first] != mp[k-x.first]) {
-                    return false;
+                if(mp.count(k-rem) > 0) {
+                    mp[k-rem]--;
+                    if(mp[k-rem] == 0){
+                        mp.erase(k-rem);
+                    }
+                } else {
+                    mp[rem]++;
                 }
             }
         }
+        if(mp.size() != 0) return false;
         return true;
     }
 };
